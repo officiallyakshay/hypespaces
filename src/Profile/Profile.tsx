32727676747436ba22847354874f5f5f99ce0dc8
Akshay Prabhakar <akshaysprabhakar@gmail.com>
2024-05-26 22:41:16 -0700

    profile work

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,10 +1,71 @@
-import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Checkbox,
+  Flex,
+  FormLabel,
+  Input,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
+
+const user = {
+  name: "Akshay Prabhakar",
+  bio: "Lover of the creator economy and oat milk lattes. Founder, Software Engineer @ Partnr",
+  member_of_rooms: ["fitness", "lifestyle"],
+};
+
+const all_rooms = ["lifestyle", "fitness", "cooking", "trends", "traveling"];
 
 export const Profile = () => {
+  const [editing, setEditing] = useState(false);
+
+  const editProfile = () => {
+    !editing ? setEditing(true) : setEditing(false);
+  };
+
   return (
-    <Flex>
-      <Text color="white">Profile</Text>
-    </Flex>
+    <Box>
+      <Flex mb="6" justify="space-between">
+        <Text color="white">My Profile</Text>
+        <Text color="white" onClick={editProfile}>
+          {editing ? "Save Profile" : "Edit Profile"}
+        </Text>
+      </Flex>
+
+      <Flex flexDir="column" mb="8">
+        <FormLabel color="white">Name</FormLabel>
+        {editing ? (
+          <Input placeholder="Add Your Name" value={user.name} color="white" />
+        ) : (
+          <Text color="white">{user.name}</Text>
+        )}
+      </Flex>
+
+      <Flex flexDir="column" mb="8">
+        <FormLabel color="white">Bio</FormLabel>
+        {editing ? (
+          <Textarea placeholder="Add Your Bio" value={user.bio} color="white" />
+        ) : (
+          <Text color="white">{user.bio}</Text>
+        )}
+      </Flex>
+
+      <Flex flexDir="column" mb="8">
+        <FormLabel color="white">Member of these rooms:</FormLabel>
+        {editing
+          ? all_rooms.map((room) => (
+              <Checkbox
+                key={room}
+                isChecked={user.member_of_rooms.includes(room)}
+              >
+                <Text color="white">{room}</Text>
+              </Checkbox>
+            ))
+          : user.member_of_rooms.map((member) => (
+              <Text color="white">{member}</Text>
+            ))}
+      </Flex>
+    </Box>
   );
 };
