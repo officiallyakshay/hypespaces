29ee2bf111f9f5212b56a98bd98a94efbec9629a
Akshay Prabhakar <akshaysprabhakar@gmail.com>
2024-05-27 22:13:21 -0700

    messages send in rooms

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ReactRouterLink, useLocation } from "react-router-dom";
 import {
   Box,
   Button,
@@ -11,20 +11,17 @@ import { HiHome, HiOutlineHome } from "react-icons/hi";
 import { PiUserFill, PiUserLight } from "react-icons/pi";
 
 export const Nav = () => {
-  const [path, setPath] = useState("");
+  const location = useLocation();
+  const path = location.pathname;
   const onHome = path === "/";
   const onProfile = path === "/profile";
-
-  useEffect(() => {
-    setPath(location.pathname);
-  }, [path]);
 
   return (
     <Box mt="auto">
       <Divider />
       <Flex justify="space-between" mt="2">
         <ChakraLink as={ReactRouterLink} to="/">
-          <Button variant="none" onClick={() => setPath("/")}>
+          <Button variant="none">
             {onHome ? (
               <HiHome fontSize="1.5rem" color="white" />
             ) : (
@@ -33,7 +30,7 @@ export const Nav = () => {
           </Button>
         </ChakraLink>
         <ChakraLink as={ReactRouterLink} to="/profile">
-          <Button variant="none" onClick={() => setPath("/profile")}>
+          <Button variant="none">
             {onProfile ? (
               <PiUserFill fontSize="1.5rem" color="white" />
             ) : (
--- src/Rooms/Rooms.tsx
+++ src/Rooms/Rooms.tsx
@@ -1,12 +1,60 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { Flex, Text } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import { IoIosArrowBack } from "react-icons/io";
 
 export const Rooms = () => {
   const { room } = useParams();
+  const navigate = useNavigate();
+  const [messages, setMessages] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSendClick = () => {
+    if (inputValue.trim()) {
+      setMessages([...messages, inputValue.trim()]);
+      setInputValue(""); // Clear the input field
+    }
+  };
+
+  const handleBackClick = () => {
+    navigate("/");
+  };
+
   return (
-    <Flex mb="4">
-      <Text color="white">{room}</Text>
+    <Flex flexDir="column" mb="4" height="100vh">
+      <Flex align="center">
+        <IoIosArrowBack
+          fontSize="1.5rem"
+          color="white"
+          onClick={handleBackClick}
+        />
+        <Text color="white" fontSize="lg">
+          {`Room: ${room}`}
+        </Text>
+      </Flex>
+
+      <Flex flexDir="column" flexGrow={1} overflowY="auto" mt="4">
+        {messages.map((message, index) => (
+          <Text key={index} color="white" mb="2">
+            {message}
+          </Text>
+        ))}
+      </Flex>
+
+      <Flex mt="auto">
+        <Input
+          color="white"
+          placeholder="Send a message!"
+          onChange={handleInputChange}
+        />
+        <Button ml="2" onClick={handleSendClick}>
+          Send
+        </Button>
+      </Flex>
     </Flex>
   );
 };
